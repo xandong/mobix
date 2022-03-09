277652e8360c25e57d91ac7e80bc33fc5f03bc3a
ventusdigital <94716639+xandong@users.noreply.github.com>
2022-03-09 03:39:06 -0300

    Update ampulheta2.js
    End of the time transition, this time respecting the laws of physics. :smirk:

--- desafio-ampulheta/ampulheta2.js
+++ desafio-ampulheta/ampulheta2.js
@@ -1,24 +1,50 @@
-const num = 10, segundo = 1000;
-let count = 0, stts = false, timer = 0;
-const space = '\40', caracter = '#';
+const space = '\40', caracter = '#', segundo = 1000;
+//let num = prompt("Insira um numero"),
+let num = 20; if(num%2!=0) num++;
 
+let countP1 = num/2,
+    countP2 = num,
+    timer = 0;
+
+// console.log(countP1,countP2)
 const ampulheta = (num) => {
     timer += segundo;
-    let meio = num/2;
-    auxNum = num;
-    auxSpace = meio-1;
+    let meio = num/2,
+    auxNum = num,
+    aux = meio;
     
+
     for (i=0; i<num; i++) {
         if (i < meio){
-            console.log(space.repeat(i), caracter.repeat(auxNum))
+            if (i==0) {
+                console.log(`${space.repeat(i+1)}${caracter.repeat(num)}`)
+            }
+            else if ((auxNum-2)/2>=countP1) {
+                console.log(`${ space.repeat(i+1) }#${ space.repeat(auxNum-2) }#`)
+            }
+            else {
+                console.log(space.repeat(i), caracter.repeat(auxNum))
+            }
             auxNum-=2;
         }
         else {
-            console.log(space.repeat(--meio), caracter.repeat(auxNum+=2))
+            --meio;
+            if (i==num-1) {
+                console.log(`${space}${caracter.repeat(num)}`)
+            }
+            else if (i<countP2) {
+                console.log(`${space.repeat(aux--)}#${space.repeat(auxNum)}#`)
+            }
+            else {
+                console.log(space.repeat(meio), caracter.repeat(auxNum+2))           
+            }
+            auxNum+=2;
         }
     }
-    ++count
-    console.log(`Contando... ${count} : ${timer}ms`)
+console.log('\n\n\n')
+--countP1;
+--countP2;
+//console.log(`P1:${countP1} P2:${countP2}`)
 }
 const intervalo = setInterval(()=>{
     ampulheta(num);
@@ -26,11 +52,3 @@ const intervalo = setInterval(()=>{
         paraIntervalo = clearInterval(intervalo)
     }
 }, segundo);
-
-//  ###### 
-//   ####
-//    ##   
-// 
-// 
-// 
-// 
